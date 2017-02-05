b225a840c111543ecf062c5bfb0cce6046e7e8f9
Anna Walasek <tuka217@gmail.com>
2017-02-05 20:32:01 +0100

    Cwiczenie lab 2

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -4,7 +4,29 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
-	
+    var params = {
+        DryRun: true || false,
+        Filters: [
+            {
+                Name: 'STRING_VALUE',
+                Values: [
+                    'STRING_VALUE',
+                    /* more items */
+                ]
+            },
+            /* more items */
+        ],
+        InstanceIds: [
+            'STRING_VALUE',
+            /* more items */
+        ],
+        MaxResults: 0,
+        NextToken: 'STRING_VALUE'
+    };
+    ec2.describeInstances(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+    });
 	
 }
 
